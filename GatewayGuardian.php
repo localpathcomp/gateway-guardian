ffa692f3fb51a4d2a463f726d13b5d9861e6d6d8
Garrick Crouch <services@localpathcomputing.com>
2020-05-30 14:34:21 -0500

    feat: add separate origin method

--- GatewayGuardian.php
+++ GatewayGuardian.php
@@ -31,9 +31,8 @@ class GatewayGuardian
         $this->headerToken = $headerToken;
         $this->sessionToken = $sessionToken;
         $this->requestMethod = $requestMethod;
-        if ($this->checkRequest === true) {
-            $this->verifyRequestMethod();
-        }
+        if ($this->checkRequest === true) $this->verifyRequestMethod();
+        $this->verifyOrigin();
         $this->verifyCSRFToken();
     }
 
@@ -48,57 +47,43 @@ class GatewayGuardian
         }
     }
 
-    protected function verifyCSRFToken()
+    protected function verifyOrigin()
     {
-
-        if ($_SERVER['REQUEST_METHOD'] === $this->requestMethod) {
-            
-            if (isset($_SERVER['HTTP_ORIGIN'])) {
-
-                $address = 'https://' . $_SERVER['SERVER_NAME'];
-
-                if (strpos($address, $_SERVER['HTTP_ORIGIN']) !== 0) {
-
-                    http_response_code(403);
-                    header('Content-Type: application/json');
-                    exit(json_encode(['error' => 'Invalid Origin header: ' . $_SERVER['HTTP_ORIGIN']]));
-
-                }
-
-            } else {
-
-                http_response_code(400);
-                header('Content-Type: application/json');
-                exit(json_encode(['error' => 'No Origin header']));
-
-            }
-
-            if (isset($_SERVER[$this->headerToken])) {
-
-                if (!hash_equals($_SESSION[$this->sessionToken], $_SERVER[$this->headerToken])) {
-
-                    http_response_code(403);
-                    header('Content-Type: application/json');
-                    exit(json_encode(['error' => 'Invalid CSRF token.']));
-
-                }
-
-            } else {
-
-                http_response_code(400);
-                header('Content-Type: application/json');
-                exit(json_encode(['error' => 'No CSRF token.']));
-
-            }
-
-        } else { 
+        if (!isset($_SERVER['HTTP_ORIGIN'])) {
 
             http_response_code(400);
             header('Content-Type: application/json');
-            exit(json_encode(['error' => 'Invalid request type']));
+            exit(json_encode(['error' => 'No Origin header']));
 
+        } else {
+
+            if (strpos('https://' . $_SERVER['SERVER_NAME'], $_SERVER['HTTP_ORIGIN']) !== 0) {
+
+                http_response_code(403);
+                header('Content-Type: application/json');
+                exit(json_encode(['error' => 'Invalid Origin header']));
+
+            }
         }
-
     }
 
+    protected function verifyCSRFToken()
+    {      
+        if (!isset($_SERVER[$this->headerToken])) {
+
+            http_response_code(400);
+            header('Content-Type: application/json');
+            exit(json_encode(['error' => 'No CSRF token.']));
+
+        } else {
+
+            if (!hash_equals($_SESSION[$this->sessionToken], $_SERVER[$this->headerToken])) {
+
+                http_response_code(403);
+                header('Content-Type: application/json');
+                exit(json_encode(['error' => 'Invalid CSRF token.']));
+
+            }
+        }
+    }
 }
